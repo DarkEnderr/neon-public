28b54d289a7fa6e89177ab990edb2b020327d5b9
Nas(Sang) <87296225+DarkEnderr@users.noreply.github.com>
2022-12-16 13:01:37 +0700

    Add files via upload

--- schemas/schema.js
+++ schemas/schema.js
@@ -3,18 +3,23 @@ const User = new mongoose.Schema({
     //_id: mongoose.Schema.Types.ObjectId,
     userid: { type: String, unique: true, required: true },
     cash: { type: Number, default: 0 },
-    weapon: { type: String, default: "https://cdn.discordapp.com/emojis/1052463689212641300.webp?size=44&quality=lossless" },
-    helmet: { type: String, default: "" },
+    health: { type: Number, default: 100 },
+    maxhealth: { type: Number, default: 100 },
+    weapon: { type: String, default: "<:a_:1052463689212641300> " },
+    helmet: { type: String, default: "<:unnamed18:1052541132623593524> " },
     chestplate: { type: String, default: "" },
-    legging: { type: String, default: "" },
+    legging: { type: String, default: "<:unnamed20:1052541123001851936> " },
     boot: { type: String, default: "" },
+    sharp: { type: Number, default: 10 },
+    protection: { type: Number, default: 3 },
     cooldowns: {
         daily: { type: Date },
         prize: { type: Date },
         beg: { type: Date },
-        boss: { type: Date }
+        setHealth: { type: Date }
     },
     level: { type: Number, default: 1 },
+    exp: { type: Number, default: 0 },
     timejoin: { type: Date },
     card: { 
         attack: { type: String },
@@ -26,15 +31,14 @@ const User = new mongoose.Schema({
 });
 
 
-const Guild = new mongoose.Schema({
-    id: { type: String, unique: true, required: true },
+const Bots = new mongoose.Schema({
     join: { type: Number, default: 0 },
     leave: { type: Number, default: 0 },
     useCommand: { type: Number, default: 0 }
 })
 
 const Users = mongoose.model('User', User);
-const Guilds = mongoose.model('Guild', Guild)
+const Guilds = mongoose.model('Guild', Bots)
 
 module.exports = {
     Users,
